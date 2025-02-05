2051c22c37f6fcc4e39585083b18ab430b84bd34
Mayur <mayurtsalunkhe@gmail.com>
2025-02-04 20:30:29 -0800

    solved internal error testing middleware

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+const openlink = () => {
+  window.open("https://google.com");
+  // console.log("Hello");
+}
 
 export default function Home() {
   return (
     <div>
       <h2>Hello</h2>
-      <Button>Click me</Button>
+      <Button onClick={openlink}>Click me</Button>
     </div>
   );
 }
--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 
-const isProtectedRoute = createRouteMatcher(['/dashboarda(.*)', '/forum(.*)']);
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/forum(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
     if (isProtectedRoute(req)) await auth.protect()
